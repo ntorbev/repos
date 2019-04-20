81f911b055a968004e98dd12746d7cb72064cd70
Nikolay Torbev <nikolay.torbev@paysafe.com>
2019-04-20 16:29:21 +0300

    login init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ClarityModule, ClrCheckboxModule, ClrInputModule, ClrPasswordModule, ClrSelectModule} from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ClrSelectModule,
+    ClrInputModule,
+    ClrPasswordModule,
+    ClrCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
